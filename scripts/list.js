544409193647179d3707ae7b43e0fa40684c4a5f
vtnw <vtnw@users.noreply.github.com>
2022-01-29 17:46:47 +0530

    Update list.js

--- scripts/list.js
+++ scripts/list.js
@@ -469,7 +469,7 @@ function getFormattedDate() {
     var d = new Date();
     d = d.getFullYear()
         + ("0" + (d.getMonth() + 1)).slice(-2)
-        + ("0" + d.getDate()).slice(-2) + " "
+        + ("0" + d.getDate()).slice(-2)
         + ("0" + d.getHours()).slice(-2)
         + ("0" + d.getMinutes()).slice(-2)
         + ("0" + d.getSeconds()).slice(-2);
